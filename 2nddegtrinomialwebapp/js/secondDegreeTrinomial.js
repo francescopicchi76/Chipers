903868834c24e701b3028b916f2d1f4b50c1aeaf
francescopicchi76 <f.picchi76@gmail.com>
2021-06-27 17:54:37 +0200

    Making JS code more compact

--- 2nddegtrinomialwebapp/js/secondDegreeTrinomial.js
+++ 2nddegtrinomialwebapp/js/secondDegreeTrinomial.js
@@ -20,72 +20,59 @@ function clearFields() {
   document.modulo.messageSign.value = "";
 }
 
-//This function analyses parameters and delta through conditional statements to define if the trinomial has got solutions
-//and how they are; then it gives as output the result, which can be just a string or a string including variables.
+//This function analyses parameters and delta to define solutions (if existing) and to study the sign of the trinomial.
 function giveSolutions(){
   let trinomial = getData();
   let risultato;
   let risultatoDelta;
+  let risultatoSegno;
+  let signOfa;
+  let inverseSign;
+
   if ((isNaN(trinomial.a)) || (isNaN(trinomial.b)) || (isNaN(trinomial.c))) {
     risultato = "Attenzione, solo valori numerici ammessi come parametri!";
+    risultatoSegno = "Attenzione, solo valori numerici ammessi come parametri!";
     risultatoDelta = ""
   }
   else {
   risultatoDelta = trinomial.d;
   if (trinomial.a==0){
     risultato = `Attenzione, deve essere a\u22600 affinché il trinomio sia di 2° grado`;
+    risultatoSegno = `Attenzione, deve essere a\u22600 affinché il trinomio sia di 2° grado`;
     risultatoDelta = ""
   }
-  else if (trinomial.a!=0 && trinomial.d<0) {
-    risultato = "Le soluzioni sono l'insieme vuoto {0}, cioè il trinomio non ha soluzioni reali, perché il \u0394 è negativo";
-  }
-  else if (trinomial.a!=0 && trinomial.d==0) {
-    risultato = `Il trinomio ha due soluzioni reali coincidenti che sono x\u2081=x\u2082=${trinomial.x1}`;
-  }
-  else if (trinomial.a!=0 && trinomial.b==0 && trinomial.c!=0 && trinomial.d>0) {
-    risultato = `Il trinomio ha due soluzioni reali e opposte che sono x\u2081=${trinomial.x1} e x\u2082=${trinomial.x2}`;
-  }
   else {
-    risultato = `Il trinomio ha due soluzioni reali e distinte che sono x\u2081=${trinomial.x1} e x\u2082=${trinomial.x2}`;
+    if (trinomial.a>0){
+      signOfa = "positivo";
+      inverseSign = "negativo"
+    }
+    else {
+      signOfa = "negativo";
+      inverseSign = "positivo";
+    }
+    if (trinomial.d<0) {
+      risultato = "Le soluzioni sono l'insieme vuoto {0}, cioè il trinomio non ha soluzioni reali, perché il \u0394 è negativo";
+      risultatoSegno = `Il trinomio è ${signOfa} per ogni valore di x, cioè ha sempre il segno di a, perché il \u0394 è negativo`;
+    }
+    else if (trinomial.d==0) {
+      risultato = `Il trinomio ha due soluzioni reali coincidenti che sono x\u2081=x\u2082=${trinomial.x1}`;
+      risultatoSegno = `Il trinomio è uguale a zero per x=${trinomial.x1} (valore delle due soluzioni coincidenti), per ogni altro valore di x il trinomio ha segno ${signOfa}`;
+    }
+    else {
+      let min = Math.min(trinomial.x1, trinomial.x2);
+      let max = Math.max(trinomial.x1, trinomial.x2);
+      risultatoSegno = `Il trinomio è ${signOfa} negli intervalli esterni x<${min} e x>${max}, mentre è ${inverseSign} nell'intervallo interno ${min}<x<${max}`;
+      if (trinomial.b==0 && trinomial.c!=0) {
+        risultato = `Il trinomio ha due soluzioni reali e opposte che sono x\u2081=${trinomial.x1} e x\u2082=${trinomial.x2}`;
+      }
+      else {
+        risultato = `Il trinomio ha due soluzioni reali e distinte che sono x\u2081=${trinomial.x1} e x\u2082=${trinomial.x2}`;
+      }
+    }
   }
-}
-document.modulo.delta.value = risultatoDelta;
-document.modulo.messageSolutions.value = risultato;
 }
 
-//This function uses parameters, delta and solutions (when existing) to define the sign of the trinomial;
-//then it gives as output the result, which can be just a string or a string including variables.
-function studySign() {
-  let trinomial = getData();
-  let risultatoSegno;
-  let signOfa;
-  let inverseSign;
-  if (isNaN(trinomial.a) || isNaN(trinomial.b) || isNaN(trinomial.c)) {
-    risultatoSegno = "Attenzione, solo valori numerici ammessi come parametri!";
-  }
-  else {
-  if (trinomial.a>=0){
-    signOfa = "positivo";
-    inverseSign = "negativo"
-  }
-  else if (trinomial.a<0) {
-    signOfa = "negativo";
-    inverseSign = "positivo";
-  }
-  if (trinomial.a==0){
-    risultatoSegno = `Attenzione, deve essere a\u22600 affinché il trinomio sia di 2° grado`;
-  }
-  else if (trinomial.a!=0 && trinomial.d<0) {
-    risultatoSegno = `Il trinomio è ${signOfa} per ogni valore di x, cioè ha sempre il segno di a, perché il \u0394 è negativo`;
-  }
-  else if (trinomial.a!=0 && trinomial.d==0){
-    risultatoSegno = `Il trinomio è uguale a zero per x=${trinomial.x1} (valore delle due soluzioni coincidenti), per ogni altro valore di x il trinomio ha segno ${signOfa}`;
-  }
-  else if (trinomial.a!=0 && trinomial.d>0){
-    let min = Math.min(trinomial.x1, trinomial.x2);
-    let max = Math.max(trinomial.x1, trinomial.x2);
-    risultatoSegno = `Il trinomio è ${signOfa} negli intervalli esterni x<${min} e x>${max}, mentre è ${inverseSign} nell'intervallo interno ${min}<x<${max}`;
-  }
-}
+document.modulo.delta.value = risultatoDelta;
+document.modulo.messageSolutions.value = risultato;
 document.modulo.messageSign.value = risultatoSegno;
 }
