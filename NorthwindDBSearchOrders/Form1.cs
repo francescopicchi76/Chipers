6de08e526474bc6b3e1d5176a4df091f3a013d27
francescopicchi76 <f.picchi76@gmail.com>
2022-01-02 13:07:41 +0100

    App to search orders in the Northwind database

---
+++ NorthwindDBSearchOrders/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace NorthwindDBSearchOrders
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection northwindConn = new SqlConnection();
+        DataTable collectedDataDT = new DataTable();
+        DataTable gridSourceDT = new DataTable();
+        string queryString, connectionString, selectedDateFrom, selectedDateTo, selectedOrderID, selectedShipper, selectedProduct, selectedEmployee;
+        
+        void clearData()
+        {
+            selectedOrderID = "";
+            selectedDateFrom = "";
+            selectedDateTo = "";
+            selectedShipper = "";
+            selectedProduct = "";
+            selectedEmployee = "";
+        }
+        void initializeForm1controls()
+        {
+            orderIDTB.Text = "";
+            orderDatefromDTP.Format = DateTimePickerFormat.Custom;
+            orderDatefromDTP.CustomFormat = " ";
+            orderDateToDTP.Format = DateTimePickerFormat.Custom;
+            orderDateToDTP.CustomFormat = " ";
+            shipperNameCB.SelectedItem = null;
+            shipperNameCB.Text = "--Select--";
+            productNameCB.SelectedItem = null;
+            productNameCB.Text = "--Select--";
+            employeeNameCB.SelectedItem = null;
+            employeeNameCB.Text = "--Select--";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clearData();
+            connectionString = "";
+
+            using (Form2 dialog = new Form2())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    connectionString = dialog.conString;
+                }
+                else
+                {
+                    Form1_Load(sender, e);
+                }
+
+                try
+                {
+                    northwindConn.ConnectionString = connectionString;
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid connection string");
+                    Form1_Load(sender, e);
+                }
+            }
+
+            SqlDataReader leggi = null;
+            SqlCommand comando = new SqlCommand("", northwindConn);
+            comando.CommandText = "select Orders.*, [Order Details].ProductID, Products.ProductName, Shippers.CompanyName, Employees.LastName from Orders inner join [Order Details] on Orders.OrderID = [Order Details].OrderID inner join Products on [Order Details].ProductID = Products.ProductID inner join Shippers on ShipVia = ShipperID inner join Employees on Orders.EmployeeID = Employees.EmployeeID";
+            
+            northwindConn.Open();
+
+            leggi = comando.ExecuteReader();
+            collectedDataDT.Load(leggi);
+
+            DataView viewShipperName = new DataView(collectedDataDT);
+            DataTable shipperName = viewShipperName.ToTable("viewShipperName", true, "CompanyName");
+            shipperNameCB.DataSource = shipperName;
+            shipperNameCB.DisplayMember = "CompanyName";
+
+            DataView viewProductName = new DataView(collectedDataDT);
+            DataTable productName = viewProductName.ToTable("viewProductName", true, "ProductName");
+            productNameCB.DataSource = productName;
+            productNameCB.DisplayMember = "ProductName";
+
+            DataView viewEmployeeName = new DataView(collectedDataDT);
+            DataTable employeeName = viewProductName.ToTable("viewEmployeeName", true, "LastName");
+            employeeNameCB.DataSource = employeeName;
+            employeeNameCB.DisplayMember = "LastName";
+
+            northwindConn.Close();
+
+            initializeForm1controls();
+        }
+
+        private void orderIDTB_TextChanged(object sender, EventArgs e)
+        {
+            selectedOrderID = orderIDTB.Text;
+        }
+        private void orderDateFromDTP_ValueChanged(object sender, EventArgs e)
+        {
+            selectedDateFrom = orderDatefromDTP.Value.Date.ToString();
+            orderDatefromDTP.Format = DateTimePickerFormat.Short;
+        }
+        private void orderDateToDTP_ValueChanged(object sender, EventArgs e)
+        {
+            selectedDateTo = orderDateToDTP.Value.Date.ToString();
+            orderDateToDTP.Format = DateTimePickerFormat.Short;
+        }
+        private void shipperNameCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedShipper = shipperNameCB.GetItemText(shipperNameCB.SelectedItem);
+        }
+        private void productNameCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedProduct = productNameCB.GetItemText(productNameCB.SelectedItem);
+        }
+        private void employeeNameCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedEmployee = employeeNameCB.GetItemText(employeeNameCB.SelectedItem);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gridSourceDT.Clear();
+            SqlDataReader leggi = null;
+            SqlCommand comando = new SqlCommand("", northwindConn);
+            queryString = "select Orders.*, [Order Details].ProductID, Products.ProductName, Shippers.CompanyName as [Shipper], Employees.LastName as [Employee] from Orders inner join [Order Details] on Orders.OrderID = [Order Details].OrderID inner join Products on [Order Details].ProductID = Products.ProductID inner join Shippers on ShipVia = ShipperID inner join Employees on Orders.EmployeeID = Employees.EmployeeID where Orders.OrderID IS NOT NULL";
+            
+            if (selectedOrderID != "")
+            {
+                queryString += " and Orders.OrderID = @OrderID";
+                comando.Parameters.AddWithValue("@OrderID", selectedOrderID);
+            }
+            if (selectedDateFrom != "")
+            {
+                queryString += " and OrderDate >= @OrderDateFrom";
+                comando.Parameters.AddWithValue("@OrderDateFrom", orderDatefromDTP.Value.Date);
+            }
+            if (selectedDateTo != "") 
+            {
+                queryString += " and OrderDate <= @OrderDateTo";
+                comando.Parameters.AddWithValue("@OrderDateTo", orderDateToDTP.Value.Date);
+            }
+            if (selectedShipper != "")
+            {
+                queryString += " and CompanyName = @CompanyName";
+                comando.Parameters.AddWithValue("@CompanyName", selectedShipper);
+            }
+            if (selectedProduct != "")
+            {
+                queryString += " and ProductName = @ProductName";
+                comando.Parameters.AddWithValue("@ProductName", selectedProduct);
+            }
+            if (selectedEmployee != "")
+            {
+                queryString += " and LastName = @LastName";
+                comando.Parameters.AddWithValue("@LastName", selectedEmployee);
+            }
+
+            comando.CommandText = queryString;
+            
+            northwindConn.Open();
+            
+            leggi = comando.ExecuteReader();
+            gridSourceDT.Load(leggi);
+            dataGridView1.DataSource = gridSourceDT;
+
+            northwindConn.Close();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clearData();
+            initializeForm1controls();
+            dataGridView1.DataSource = null;
+        }
+    }
+}
---
+++ NorthwindDBSearchOrders/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NorthwindDBSearchOrders
+{
+    public partial class Form2 : Form
+    {
+        public string conString = "";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conString = getConnectionStringTB.Text;
+        }
+    }
+}
